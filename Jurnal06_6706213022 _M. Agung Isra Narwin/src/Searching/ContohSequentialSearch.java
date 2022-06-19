e56f25084a8376146f54a9716989b736842d16ff
M. Agung Isra Narwin <91040756+agungisra@users.noreply.github.com>
2022-06-19 19:57:57 +0700

    Add files via upload

---
+++ Jurnal06_6706213022 _M. Agung Isra Narwin/src/Searching/ContohSequentialSearch.java
@@ -0,0 +1,39 @@
+package Searching;
+
+public class ContohSequentialSearch {
+    private int[] allData = new int[]{30, 4, 7, 10, 8, 100, 33};
+
+    private void tampilkanData(){
+        for (int data : allData) {
+            System.out.print(data + " ");
+        }
+        System.out.println();
+    }
+
+    private void searching(int karakter){
+        int x = 0;
+        boolean ketemu = false;
+        for (int i = x; i < allData.length; i++) {
+            if(karakter.equals(allData[i])){
+                ketemu = true;
+                x = i;
+            }
+        }
+        if(ketemu){
+            System.out.println("Data berada pada urutan ke - "+(x+1));
+        } else {
+            System.out.println("Data Tidak Ditemukan");
+        }
+    }
+
+    public static void main(String[] args) {
+        ContohSequentialSearch obj = new ContohSequentialSearch();
+
+//        Untuk menampilkan data pada Array
+        obj.tampilkanData();
+
+//        Melakukan pencarian data
+        obj.searching(4);
+    }
+}
+
